53cd24aed5870c4b12fa562125f0653a5aed473f
Jarred Hone <honejarred@gmail.com>
2015-06-09 16:59:01 +1200

    Made progress on table row headers

--- js/table.js
+++ js/table.js
@@ -2,17 +2,25 @@
 
 //Column headers/definitions
 var columns = [
-{head: 'Team Name', cl: 'title', html: ƒ('title')}
-{}
-
+{head: 'Team Name', cl: 'title', html: ƒ('name')}
+{head: 'Home Wins', cl: 'num', html: f('homewins')}
+{head: 'Away Wins', cl: 'num', html: f('awaywins')}
+{head: 'Wins Against Same Country', cl: 'num', html: f('samecountry')}
+{head: 'Wins Against Different Country', cl: 'num', html: f('diffcountry')}
 ];
 
 
 
-//Store the table rows - Team Name, Home Wins, Away Wins, Wins Against same Country, Wins against different country
+//Store the table rows - Team Name, Home Wins, Away Wins, Wins Against same Country, Wins against different country. 
 var teams = [];
 
 
-//Grab the data from the loaddata script and
+//Grab the data from the loaddata script and fill the teams array
 function populateTeams(){
+
+
+
+
+
+
 };
