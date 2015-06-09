0a1bbaaf4c3bd96c3ec8abf664784a4dfaf9a7b4
Jarred Hone <honejarred@gmail.com>
2015-06-09 16:40:17 +1200

    Added convenience wrappers, Started implementing table

---
+++ js/table.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//Column headers/definitions
+var columns = [
+{head: 'Team Name', cl: 'title', html: ƒ('title')}
+{}
+
+];
+
+
+
+//Store the table rows - Team Name, Home Wins, Away Wins, Wins Against same Country, Wins against different country
+var teams = [];
+
+
+//Grab the data from the loaddata script and
+function populateTeams(){
+};
